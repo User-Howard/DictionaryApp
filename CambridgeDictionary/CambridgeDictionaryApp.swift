3c00b745d2ad889a574c45c7fe5b15ae74441bf7
User-Howard <howard20060216@gmail.com>
2022-07-17 20:26:46 +0800

    Initial Commit

---
+++ CambridgeDictionary/CambridgeDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CambridgeDictionaryApp.swift
+//  CambridgeDictionary
+//
+//  Created by 吳浩瑋 on 2022/7/17.
+//
+
+import SwiftUI
+
+@main
+struct CambridgeDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CambridgeDictionary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CambridgeDictionary
+//
+//  Created by 吳浩瑋 on 2022/7/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
